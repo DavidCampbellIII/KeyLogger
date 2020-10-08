1b3ed173f867dc4cbd936d9910f9d3ac43fe8b2a
David Campbell III <lapidistcubed@gmail.com>
2020-10-08 14:51:27 -0500

    Improved input handling

--- src/core/Input.java
+++ src/core/Input.java
@@ -3,9 +3,25 @@ package core;
 import java.util.Scanner;
 
 public class Input {
-	//TODO add input validation, such as validateInt, validateOneWord, validateTwoWords etc.
 
 	private static Scanner input = new Scanner(System.in);
+
+	public static double validateDouble(String improperInputMessage)
+	{
+		do
+		{
+			try
+			{
+				double num = Double.parseDouble(input.nextLine().trim());
+				return num;
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println(improperInputMessage);
+			}
+		}
+		while(true);
+	}
 
 	public static int validateInt(String improperInputMessage)
 	{
@@ -26,16 +42,94 @@ public class Input {
 
 	public static int validateInt(String improperInputMessage, int[] choices)
 	{
-		return 0; //TODO
+		do
+		{
+			try
+			{
+				int num = Integer.parseInt(input.nextLine().trim());
+				for(int i = 0; i < choices.length; i++)
+				{
+					int current = choices[i];
+					if(current == num)
+					{
+						return num;
+					}
+				}
+				System.out.println(improperInputMessage);
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println(improperInputMessage);
+			}
+		}
+		while(true);
 	}
 
+	//Inclusive ranges
 	public static int validateInt(String improperInputMessage, int minRange, int maxRange)
 	{
-		return 0; //TODO
+		do
+		{
+			try
+			{
+				int num = Integer.parseInt(input.nextLine().trim());
+				if(num >= minRange && num <= maxRange)
+				{
+					return num;
+				}
+				System.out.println(improperInputMessage);
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println(improperInputMessage);
+			}
+		}
+		while(true);
 	}
 
 	public static String validateString(String improperInputMessage, String[] choices)
 	{
-		return null; //TODO
+		do
+		{
+			String answer = input.nextLine().trim();
+			for(int i = 0; i < choices.length; i++)
+			{
+				String current = choices[i];
+				if(current.equals(answer))
+				{
+					return answer;
+				}
+			}
+			System.out.println(improperInputMessage);
+		}
+		while(true);
+	}
+
+	public static String validateOneWord(String improperInputMessage)
+	{
+		do
+		{
+			String answer = input.nextLine().trim();
+			if(!answer.contains(" "))
+			{
+				return answer;
+			}
+			System.out.println(improperInputMessage);
+		}
+		while(true);
+	}
+
+	public static String validateTwoWord(String improperInputMessage)
+	{
+		do
+		{
+			String answer = input.nextLine().trim();
+			if(answer.contains(" ") && answer.indexOf(' ') == answer.lastIndexOf(' '))
+			{
+				return answer;
+			}
+			System.out.println(improperInputMessage);
+		}
+		while(true);
 	}
 }
